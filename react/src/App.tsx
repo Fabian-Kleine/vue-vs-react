4329bcd7b0dd21e0843523bae708bbcd21ee847f
Fabian Kleine <fabian.kleine@rhiem.com>
2025-04-28 08:32:04 +0200

    feat: add kanban col

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,9 +1,28 @@
+import KanbanCol from "./components/KanbanCol"
+
 function App() {
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold">Kanban Board</h1>
-    </div>
+    <main className="min-h-screen bg-gray-100 p-4 overflow-x-auto">
+      <h1 className="text-4xl text-center font-bold">Kanban Board</h1>
+      <div className="flex gap-4 mt-4">
+        <KanbanCol title="Backlog">
+          <span>test</span>
+        </KanbanCol>
+        <KanbanCol title="To Do">
+          <span>test</span>
+        </KanbanCol>
+        <KanbanCol title="In Progress">
+          <span>test</span>
+        </KanbanCol>
+        <KanbanCol title="Review">
+          <span>test</span>
+        </KanbanCol>
+        <KanbanCol title="Done">
+          <span>test</span>
+        </KanbanCol>
+      </div>
+    </main>
   )
 }
 
