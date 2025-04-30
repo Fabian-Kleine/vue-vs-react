8f176ae3534e3795370f930b33496b699678aac4
R6-Creative-Hub <96775695+Fabian-Kleine@users.noreply.github.com>
2025-05-01 00:24:16 +0200

    fix: external link href for prod

--- react/src/components/ExternalLinks.tsx
+++ react/src/components/ExternalLinks.tsx
@@ -1,7 +1,7 @@
 export default function ExternalLinks() {
     return (
         <div className="fixed left-5 bottom-10 flex flex-col gap-2">
-            <a href="vue" className="group h-6 w-fit flex justify-start items-center gap-2 p-2 bg-[#243444] hover:bg-[#31475E] transition-all overflow-hidden rounded-lg shadow-lg text-white box-content max-w-5 hover:max-w-52">
+            <a href="../vue" className="group h-6 w-fit flex justify-start items-center gap-2 p-2 bg-[#243444] hover:bg-[#31475E] transition-all overflow-hidden rounded-lg shadow-lg text-white box-content max-w-5 hover:max-w-52">
                 <img src="https://cdn.simpleicons.org/vue.js/vue.js" alt="Vue Icon" width={20} />
                 <span className="font-medium tracking-wide opacity-0 group-hover:opacity-100 text-nowrap">Visit Vue Version</span>
             </a>
--- vue/src/components/ExternalLinks.vue
+++ vue/src/components/ExternalLinks.vue
@@ -1,6 +1,6 @@
 <template>
     <div className="fixed left-5 bottom-10 flex flex-col gap-2">
-        <a href="react"
+        <a href="../react"
             class="group flex justify-start items-center gap-2 p-2 bg-[#23272F] hover:bg-[#1A1D23] transition-all rounded-lg shadow-lg text-white cursor-pointer box-content max-w-5 hover:max-w-52">
             <img src="https://cdn.simpleicons.org/react/react" alt="React Icon" width="20" />
             <span class="font-medium tracking-wide opacity-0 group-hover:opacity-100 text-nowrap">Visit React
